14ecccde621cd2e27efdeb4d4d045bc7e49c0857
Roberto Ribes <roberto.ribes@tgw-group.com>
2022-10-13 21:18:16 +0200

    Example ServiceBus Reader

---
+++ Az204.ServiceBus.Reader/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Az204.ServiceBus.Reader;
+
+Console.WriteLine("Service Bus Publisher!");
+
+ServiceBusReaderApplication application = new ServiceBusReaderApplication();
+
+await application.RunQueue("messagequeue", 1);
---
+++ Az204.ServiceBus.Reader/ServiceBusReaderApplication.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using Azure;
+using Azure.Messaging.ServiceBus;
+
+namespace Az204.ServiceBus.Reader
+{
+    public class ServiceBusReaderApplication
+    {
+        public async Task RunQueue(string queueName, int runningTimeInMinutes)
+        {
+            string connectionString =
+                "Endpoint=sb://sbnamespaceroberto.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=FNd+WzIf5gcw/bW1YcOay7pzzfzRu26gcZ7TRy6B97Q=";
+
+            ServiceBusClient client = new ServiceBusClient(connectionString);
+            ServiceBusProcessor? processor = client.CreateProcessor(queueName);
+
+            processor.ProcessMessageAsync += ProcessorOnProcessMessageAsync;
+            processor.ProcessErrorAsync += ProcessorOnProcessErrorAsync;
+
+            await processor.StartProcessingAsync();
+
+            Console.WriteLine($"Wait for {runningTimeInMinutes} minutes");
+
+            int i = 0;
+            while (i < runningTimeInMinutes)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(30));
+                i++;
+            }
+            
+            await processor.StopProcessingAsync();
+        }
+
+        private Task ProcessorOnProcessErrorAsync(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessorOnProcessMessageAsync(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+    }
+}
